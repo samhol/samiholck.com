0fa9b5a40330cdec418747a6daaaccee56b36c35
samhol <sami.holck@gmail.com>
2018-03-21 02:04:14 +0200

    Update

--- samiholck/templates/menus/siteNav.php
+++ samiholck/templates/menus/siteNav.php
@@ -9,8 +9,8 @@ if (Config::instance()->get('ROOT_URL') !== Config::instance()->get('CURRENT_URL
  // echo $button;
 }
 $buttonGroup = new ButtonGroup();
-$buttonGroup->appendHyperlink(Config::instance()->get('ROOT_URL'), FontAwesome::get('fas fa-home').' home', '_self')->addCssClass('alert', 'radius');
-$buttonGroup->appendHyperlink(Config::instance()->get('ROOT_URL').'who', FontAwesome::get('fas fa-info-circle').' Who?', '_self')->addCssClass('alert', 'radius');
-$buttonGroup->appendHyperlink(Config::instance()->get('ROOT_URL').'why', FontAwesome::get('fas fa-info-circle').' Why?', '_self')->addCssClass('alert', 'radius');
+$buttonGroup->appendHyperlink(Config::instance()->get('ROOT_URL'), FontAwesome::get('fas fa-home').' home')->addCssClass('alert', 'radius');
+$buttonGroup->appendHyperlink(Config::instance()->get('ROOT_URL').'who', FontAwesome::get('fas fa-info-circle').' Who?')->addCssClass('alert', 'radius');
+$buttonGroup->appendHyperlink(Config::instance()->get('ROOT_URL').'why', FontAwesome::get('fas fa-info-circle').' Why?')->addCssClass('alert', 'radius');
 
   echo $buttonGroup->addCssClass('page-navi', 'radius');
