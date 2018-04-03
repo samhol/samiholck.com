e19b5c70c2e2017e8beb6018207408f84d6dcd87
Sami Holck <sami.holck@gmail.com>
2018-04-03 11:09:25 +0300

    Update

--- contact-form/form.js
+++ contact-form/form.js
@@ -7,8 +7,15 @@ $(function () {
     console.log("Loading....");
     $cont.load('http://www.samiholck.com/contact-form/form-html.php', function () {
       abideForm = new Foundation.Abide($cont.find('form'));
+
     });
   };
-
+  loadCaptcha = function () {
+    grecaptcha.render('example3', {
+      'sitekey': 'your_site_key',
+      'callback': verifyCallback,
+      'theme': 'dark'
+    });
+  };
   loadAbide();
 });
