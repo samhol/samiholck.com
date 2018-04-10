2f96f1f1d244e65779f96aecdf6e48335bbc3e06
samhol <sami.holck@gmail.com>
2018-04-10 20:19:37 +0300

    Updates

--- samiholck/templates/carousels/content/skills-md.php
+++ samiholck/templates/carousels/content/skills-md.php
@@ -307,13 +307,12 @@ use Samiholck\skillLevelBar as bar;
   is developed and maintained by an open community of developers under the 
   auspices of the Apache Software Foundation.
 
-  * https://en.wikipedia.org/wiki/Apache_HTTP_Server
-
   <hr>
   <div class="button-group small align-right">
     <a class="button" href="https://httpd.apache.org/"><i class="fas fa-home"></i> Official site</a>
     <a class="button" href="https://httpd.apache.org/docs/"><i class="fas fa-book"></i> Docs</a>
     <a class="button" href="https://httpd.apache.org/download.cgi"><i class="fas fa-download"></i> Downloads</a>
+    <a class="button" href="https://en.wikipedia.org/wiki/Apache_HTTP_Server"><i class="fas fa-download"></i> Wikipedia</a>
   </div>
 
 </div>
