e6c99d0831345e7610c62d9b9b568bc711361f55
samhol <sami.holck@gmail.com>
2018-10-29 16:33:56 +0200

    Updates

--- index.php
+++ index.php
@@ -11,38 +11,38 @@ $redirect = filter_input(INPUT_SERVER, 'REDIRECT_URL', FILTER_SANITIZE_URL);
 
 $cacheSuffix = str_replace(['.', '/'], ['-', ''], $redirect) . "-cache";
 
-if ($outputCache->start("$cacheSuffix-head") === false) {
+//if ($outputCache->start("$cacheSuffix-head") === false) {
   require_once('samiholck/templates/blocks/head.php');
-  $outputCache->end();
-}
+//  $outputCache->end();
+//}
 ?>
 <?php
-if ($outputCache->start("$cacheSuffix-topbar") === false) {
+//if ($outputCache->start("$cacheSuffix-topbar") === false) {
   include('samiholck/templates/logo-area.php');
   include('samiholck/templates/menus/topBar.php');
-  $outputCache->end();
-}
+//  $outputCache->end();
+//}
 ?>
 <div class="grid-container"> 
   <div class="grid-x">
 
     <div class="mainContent small-auto cell"> 
       <?php
-      $man_cache = "$cacheSuffix-content";
-      if ($outputCache->start($man_cache) === false) {
+      //$man_cache = "$cacheSuffix-content";
+      //if ($outputCache->start($man_cache) === false) {
         $router->execute();
-        $outputCache->end();
-      }
+     //   $outputCache->end();
+     // }
       ?>
     </div>
   </div>
 </div>
 <?php
-if ($outputCache->start('footer') === false) {
+//if ($outputCache->start('footer') === false) {
   include('samiholck/templates/blocks/footer.php');
   include('samiholck/templates/backToTopButton.php');
-  $outputCache->end();
-}
+//  $outputCache->end();
+//}
 
 $html->documentClose();
 
--- samiholck/common/all.php
+++ samiholck/common/all.php
@@ -5,4 +5,4 @@ require_once 'functions.php';
 require_once 'loaders.php';
 require_once 'config.php';
 require_once 'menus.php';
-require_once 'cache.php';
+//require_once 'cache.php';
